cde1827f1148962ad17ac73a5b77a8bfee89e163
jasonMisaq <powergame_coder2@yahoo.com>
2017-12-17 01:32:58 +0330

    Added a gridview

---
+++ app/src/main/java/com/sematec_co/tictactoe/GridAdapter.kt
@@ -0,0 +1,29 @@
+package com.sematec_co.tictactoe
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+
+/**
+ * Created by johndoe on 12/17/17.
+ */
+class GridAdapter : BaseAdapter() {
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val imageView: ImageView
+
+    }
+}
